1b91ee85e033ffdf22e0fa68238f544c80ab5e17
Kaz <kazvorpal@gmail.com>
2021-02-01 16:25:25 -0500

    Did that commit?

--- index.js
+++ index.js
@@ -47,8 +47,4 @@ app.get('/', function(req, res){
 	res.send(weatherdata);
 })
 
-var server = app.listen((process.env.PORT||8080), function() {
-	var host = server.address().address
-	var port = server.address().port
-	console.log("SCP Demo listening at http://%s:%s", host, port)
-})
+var server = app.listen(process.env.PORT||8080)
