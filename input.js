dcf3a2514e80325fea26370e08b16422b40f3180
Kaz <kazvorpal@gmail.com>
2021-02-01 15:29:34 -0500

    Quick test

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 const url="http://api.openweathermap.org/data/2.5/forecast?id=4330145&cnt=3&appid=98e80921c229ff15439ec677b6763e6a&units=imperial"
 const http = require('http');
 let getweather = () => {
+	// Get information from Open Weather
 	let weather = [];
 	http.get(url, (resp) => {
 		let data = '';
