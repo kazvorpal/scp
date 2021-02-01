4197f9ce0676ce78e534d7d4f1ed2a4d642519be
Kaz <kazvorpal@gmail.com>
2021-02-01 16:05:22 -0500

    grrr...heroku

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/', function(req, res){
 	res.send(weatherdata);
 })
 
-var server = app.listen(8080, function() {
+var server = app.listen(process.env.PORT||8080, function() {
 	var host = server.address().address
 	var port = server.address().port
 	console.log("SCP Demo listening at http://%s:%s", host, port)
