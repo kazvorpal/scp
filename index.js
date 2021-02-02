f03b81e338ab931a8789fe04499d632699ff8025
Kaz <kazvorpal@gmail.com>
2021-02-01 22:37:39 -0500

    forced?

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get('/', function(req, res){
 console.log(process);
 
 
-app.set('port', process.env.PORT);
+app.set('port', 8081);
 var server = app.listen(app.get('port'), function() {
 	var host = server.address().address
 	var port = server.address().port
